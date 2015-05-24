8545a4b938b4217ffe91877afc57b4a43a0847c9
on-three <on.three.email@gmail.com>
2015-05-24 10:48:18 -0800

    initial commit

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AustinHarris.JsonRpc;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class ExampleCalculatorService : JsonRpcService
+    {
+        [JsonRpcMethod]
+        private double add(double l, double r)
+        {
+            return l+r;
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        static ExampleCalculatorService cs = new ExampleCalculatorService();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+    }
+
+}
